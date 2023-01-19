b678f9974f1525ab833a70920cda873b9b321c34
B10L0G4 <vandyyh@gmail.com>
2023-01-19 15:13:25 -0300

    add delete

--- todo-curso-react/src/components/FirstComponentes.js
+++ todo-curso-react/src/components/FirstComponentes.js
@@ -1,9 +1,20 @@
-const FirstComponet =  () => {
-  return (
-    <div className='firstcomponent'>
-      <p> Primeiro Componente </p>
-    </div>
-  )
-}
+/* eslint-disable no-sequences */
+/* eslint-disable no-undef */
+// import '../App'
+// const API = "http://localhost:5000";
+// const handleDelete =  async (id) => {
+//   return (
+//     <div className='handleDelete'>
+//       const handleDelete = async (id) ={'>'} {
+//       await fetch(API + "/todos" + id, {
+//       method: "DELETE"
+//       }),
+//   setTodos((prevState) => prevState.filter((todo)=> todo.id !== id))
 
-export default FirstComponet;
+// }
+//     </div>
+//   )
+// }
+
+
+// export default handleDelete;
